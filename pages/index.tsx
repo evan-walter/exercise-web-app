833b5e5a82ab1be6ac0204526d599dc0a837e1c7
Evan Walter <evan.walter.software@gmail.com>
2023-03-01 16:06:16 -0800

    Set up dark theme style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
+import ThemeButton from '/components/ThemeButton'
 import Timer from '/components/Timer'
 
 export default function Home() {
@@ -12,9 +12,13 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <Timer />
-      </main>
+
+      <ThemeProvider attribute='class'>
+        <main className='mx-auto h-screen w-full'>
+          <ThemeButton />
+          <Timer />
+        </main>
+      </ThemeProvider>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    'app/**/*.{js,ts,jsx,tsx}',
+    'pages/**/*.{js,ts,jsx,tsx}',
+    'components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '0',
+      },
+    },
     extend: {},
   },
   plugins: [],
